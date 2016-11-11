52346cd3030222cc31ee649cc9173baa0bc57dd7
Dan <dannguyen90@gmail.com>
2016-11-10 21:30:36 -0500

    added form to add a new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,19 @@ class ProductsController < ApplicationController
     @products = Product.all
     render 'all_products.html.erb'
   end
+
+  def add_product
+    render 'add_product.html.erb'
+  end
+
+  def added_product_confirmation
+    contact = Product.new(
+      name: params["name"], 
+      price: params["price"], 
+      image: params["image"], 
+      description: params["description"]
+      )
+    contact.save
+    render 'added_product_confirmation.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/products' => 'products#display_all_products'
+  get 'addproduct' => 'products#add_product'
+  post 'addedproduct' => 'products#added_product_confirmation'
 end
